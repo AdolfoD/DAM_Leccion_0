fc11f6289d0a34a84d0d21b7a4b53ec6474b5d7e
AdolfoD <adolfodesixto17@gmail.com>
2025-01-28 18:52:29 -0600

    Version_02: Instrucciones Basicas en boton y caja de texto

--- DAM_Leccion_ADSQ/MainPage.xaml.cs
+++ DAM_Leccion_ADSQ/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_ADSQ
+﻿using DAM_Leccion_ADSQ.Model;
+
+namespace DAM_Leccion_ADSQ
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,7 @@
         public MainPage()
         {
             InitializeComponent();
+            Ejecutar();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +22,17 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        public void Ejecutar()
+        {
+            PersonaModel personaModel = new PersonaModel();
+
+            personaModel.Nombre = "Hola Bebé";
+            txtNombre3.Text= personaModel.Nombre;
+        }
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema", "Se ah guardado el registro en DB", "Aceptar");
         }
     }
 
